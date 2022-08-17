9ceb9ee34ccd3a0cc1da0836bdfcbe9966ab843a
Adonis Amarante <adonisamarante@gmail.com>
2022-08-17 20:22:07 -0300

    feat: order component and orders list

---
+++ src/components/Order.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { HStack, Text, useTheme, Box, VStack } from 'native-base';
+
+export type OrderProps = {
+  id: string;
+  patrimony: string;
+  when: string;
+  status: 'open' | 'closed';
+}
+
+type Props = {
+  data: OrderProps;
+}
+
+export function Order({ data, ...rest }: Props) {
+  const { colors } = useTheme();
+  const statusColor = data.status === 'open' ? colors.secondary[700] : colors.green[300];
+
+  return (
+    <HStack
+      bg='gray.600'
+      mb={4}
+      alignItems='center'
+      justifyContent='space-between'
+      rounded='sm'
+      overflow='hidden'
+    >
+      <Box h='full' w={2} bg={statusColor} />
+      <VStack flex={1} my={5} ml={5}>
+        <Text color='white' fontSize='md'>
+          Patrimonio: {data.patrimony}
+        </Text>
+      </VStack>
+    </HStack>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react';
-import { HStack, IconButton, VStack, useTheme, Text, Heading } from 'native-base';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList } from 'native-base';
 import { SignOut } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg';
 
 import { Filter } from '../components/Filter';
+import { Order, OrderProps } from '../components/Order';
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+  const [orders, steOrders] = useState<OrderProps[]>([{
+    id: '123',
+    patrimony: '123456',
+    when: '18/07/2022 às 10:00',
+    status: 'open'
+  }])
 
   const { colors } = useTheme();
 
@@ -42,13 +49,23 @@ export function Home() {
           <Filter
             type='open'
             title="em andamento"
+            onPress={() => setStatusSelected('open')}
+            isActive={statusSelected === 'open'}
           />
 
           <Filter
             type='closed'
             title="finalizado"
+            onPress={() => setStatusSelected('closed')}
+            isActive={statusSelected === 'closed'}
           />
         </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+        />
       </VStack>
     </VStack>
   );
