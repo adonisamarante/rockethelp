a4fcc7567c52cbb812e7dd3c849f137c90b0c98f
Adonis Amarante <adonisamarante@gmail.com>
2022-08-18 19:03:09 -0300

    feat: order list item enhancements and empty state

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList } from 'native-base';
-import { SignOut } from 'phosphor-react-native';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+import { SignOut, ChatTeardropText } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg';
 
 import { Filter } from '../components/Filter';
+import { Button } from '../components/Button';
 import { Order, OrderProps } from '../components/Order';
 
 export function Home() {
@@ -65,7 +66,20 @@ export function Home() {
           data={orders}
           keyExtractor={item => item.id}
           renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color='gray.300' fontSize='xl' mt={6} textAlign='center'>
+                Você ainda não possui {'\n'}
+                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+              </Text>
+            </Center>
+          )}
         />
+
+        <Button title='Nova solicitação' />
       </VStack>
     </VStack>
   );
