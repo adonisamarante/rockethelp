6907c2c20fd5797a456b7c304b2b661adb1672c6
Adonis Amarante <adonisamarante@gmail.com>
2022-09-16 08:06:30 -0300

    feat: sending information to firestore

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,15 +1,54 @@
+import { useEffect, useState } from 'react';
 import { VStack, Text } from 'native-base';
 import { useRoute } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+import { dateFormat } from '../utils/firestoreDateFormat';
 
 import { Header } from '../components/Header';
+import { OrderProps } from '../components/Order';
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
 
 type RouteParams = {
   orderId: string;
 }
 
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
+}
+
 export function Details() {
+  const [solution, setSolution] = useState('')
+  const [isLoading, setIsLoading] = useState(true);
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails)
+
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
+
+  useEffect(() => {
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .get()
+    .then((doc) => {
+      const { patrimony, description, status, created_at, closed_at, solution } = doc.data();
+
+      const closed = closed_at ? dateFormat(closed_at) : null;
+
+      setOrder({
+        id: doc.id,
+        patrimony,
+        description,
+        status,
+        solution,
+        when: dateFormat(created_at),
+        closed
+      });
+
+      setIsLoading(false);
+    })
+  }, []);
 
   return (
     <VStack flex={1} bg='gray.700'>
